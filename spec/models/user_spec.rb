7cb40ba17fe132a5c2825d819d1e66e11a4319a0
Jv Pabalan <jvv.pabalan@outlook.com>
2013-10-09 23:26:56 +0800

    Testing for error messages and flash message

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -70,7 +70,7 @@ describe User do
     it { should_not be_valid }
   end
 
-  describe "when email address ia already taken" do
+  describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
       user_with_same_email.email = @user.email.upcase
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -20,6 +20,82 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "when name is blank" do
+        before do 
+         fill_in "Name", with: "" 
+         click_button submit
+        end
+
+        it { should have_content("Name can't be blank") }
+      end
+
+      describe "when email is blank" do
+        before do 
+          fill_in "Email", with: ""
+          click_button submit
+        end
+
+        it { should have_content("Email can't be blank") }
+      end
+
+
+      describe "when email is already taken" do
+        before do
+          user = User.create(name: "Example User", email: "example@mail.com", 
+                            password: "foobar", password_confirmation: "foobar")
+          fill_in "Email", with: user.email
+          click_button submit
+        end
+
+        it { should have_content("taken") }
+      end
+
+      describe "when password is blank" do
+        before do
+          fill_in "Password", with: ""
+          click_button submit
+        end
+
+        it { should have_content("Password can't be blank")}
+      end
+
+      describe "when password is less than six characters" do
+        before  do
+          fill_in "Password", with: "aaaaa"
+          click_button submit
+        end
+
+        it { should have_content("Password is too short") }
+      end
+
+      describe "when Confirmation is blank" do
+        before do 
+          fill_in "Password", with: "aaaaaa"
+          fill_in "Confirmation", with: ""
+          click_button submit
+        end
+
+        it { should have_content("Password confirmation can't be blank") }
+      end
+
+      describe "when confirmation doesn't match password" do
+        before do
+          fill_in "Password", with: "aaaaaa"
+          fill_in "Confirmation", with: "bbbbbb"
+          click_button submit
+        end
+
+        it { should have_content("Password confirmation doesn't match Password")}
+      end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title("Sign up") }
+        it { should have_content("error") }
+      end
+
     end
 
     describe "signup with valid information" do
@@ -29,8 +105,17 @@ describe "User pages" do
           fill_in "Password", with: "foobar"
           fill_in "Confirmation", with: "foobar"
       end
+
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
